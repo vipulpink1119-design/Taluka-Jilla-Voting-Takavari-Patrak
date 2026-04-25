ec863d89e36e725a07b43ed4a1a0152495d8b071
vipulpink1119-design <vipulpink1119@gmail.com>
2026-04-25 20:12:50 +0530

    Update sw-1.js

--- sw-1.js
+++ sw-1.js
@@ -1,7 +1,7 @@
 const CACHE = 'election-v1';
 const ASSETS = [
   './index.html',
-  './manifest.json',
+  './manifest-2.json',
   'https://fonts.googleapis.com/css2?family=Hind+Vadodara:wght@400;600;700;800&display=swap',
   'https://cdnjs.cloudflare.com/ajax/libs/html2canvas/1.4.1/html2canvas.min.js'
 ];
